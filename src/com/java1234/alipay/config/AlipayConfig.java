fa01ee8e2ac63a100e5c8241b7e2ae310468c192
admin <869893469@qq.com>
2016-10-10 10:45:55 +0800

    第一个mybatis项目

---
+++ src/com/java1234/alipay/config/AlipayConfig.java
@@ -0,0 +1,20 @@
+package com.java1234.alipay.config;
+
+public class AlipayConfig {
+	// 合作身份者ID，以2088开头由16位纯数字组成的字符串
+	public static String partner = "";
+	//商户私钥
+	public static String private_key="";
+	//支付宝公钥
+	public static String ali_public_key = "";
+	//日志路径
+	public static String log_path = "";
+	//字符编码格式集
+	public static String input_charset = "";
+	//版本号
+	public static String version = "";
+	//商户收款账号
+	public static final String SELLER ="";
+	//后台回调地址
+	public static final String notify_url = "";
+}
